0e18b3310f3ed0d9268bd01801a60dd8f0e2ea35
kmalikov <malikov917@gmail.com>
2018-01-11 23:39:15 +0300

    отправка сообщений "Связаться с нами" + стили

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {HttpService} from "../http.service";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class FooterComponent {
+  form: FormGroup;
+  constructor(private httpService: HttpService) {
+    this.form = new FormGroup({
+      author: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      body: new FormControl('', Validators.required),
+      category: new FormControl('Обратная связь')
+    })
   }
 
+  sendFeedback() {
+    console.log(this.form);
+    this.httpService.postMessage(this.form).subscribe()
+  }
 }
