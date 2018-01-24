997cc29dd21345a73893098282c1a5701d71ddd1
kmalikov <malikov917@gmail.com>
2018-01-24 10:42:35 +0300

    1.  добавил спинер на загрузку первой страницы 2.  теперь страница пагинатора сбрасывается, когда переходим по категориям 3. сделал модалку как галерея для картинок 4. кол-во продуктов подправил (надо сделать обновление информации в localstorage и по это информации сделать "общую стоимость")

--- src/app/body/single-product/product-item/product-item.component.ts
+++ src/app/body/single-product/product-item/product-item.component.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/switchMap';
 import {InitMain} from "../../../../assets/js/main.init";
 import {AuthenticationService} from "../../../autentication.service";
 import {AppStore} from "../../../app.store.service";
+import {MatDialog} from "@angular/material";
+import {ImageGalleryModalComponent} from "./image-gallery-modal/image-gallery-modal.component";
 
 @Component({
   selector: 'app-product',
@@ -18,70 +20,71 @@ export class ProductItemComponent implements OnInit {
   localArray = [];
 
   constructor(private activatedRoute: ActivatedRoute, private httpService: HttpService,
-              private auth: AuthenticationService, private store: AppStore) {
+              private auth: AuthenticationService, private store: AppStore,
+              public dialog: MatDialog) {
   }
 
 
+  ngOnInit() {
+    this.activatedRoute.queryParamMap
+        .switchMap((params: ParamMap) => {
+          const cache = JSON.parse(localStorage.getItem(this.auth.getUserUsername()));
+          if (cache) {
+            const item = cache.find(object => object._id == params.get('id'));
+            if (item) this.localArray.push(item);
+          }
+          return this.httpService.getProductById(params.get('id'))
+        })
+        .subscribe(data => {
+          this.product = data;
+          this.setScripts();
+        });
+  }
 
-ngOnInit()
-{
-  this.activatedRoute.queryParamMap
-    .switchMap((params: ParamMap) => {
-      const cache = JSON.parse(localStorage.getItem(this.auth.getUserUsername()));
-      if (cache) {
-        const item = cache.find(object => object._id == params.get('id'));
-        if (item) this.localArray.push(item);
+  setScripts() {
+    setTimeout(() => {
+      InitMain.elevateZoomActive();
+      // InitMain.slickSingleProductZoomImage();
+      InitMain.cartPlusMinusButton();
+    }, 200);
+  }
+
+  openModalGallery(observableImage) {
+    this.dialog.open(ImageGalleryModalComponent, {data: [observableImage]});
+  }
+
+  addToBasket(id) {
+    const cache = JSON.parse(localStorage.getItem(this.auth.getUserUsername()));
+
+    if (this.localArray.length) {
+      this.localArray = [];
+      cache.splice(cache.findIndex(data => data._id == id), 1);
+      if (!cache.length) {
+        localStorage.clear();
+      } else {
+        localStorage.setItem(this.auth.getUserUsername(), JSON.stringify(cache));
       }
-      return this.httpService.getProductById(params.get('id'))
-    })
-    .subscribe(data => {
-      this.product = data;
-      this.setScripts();
-    });
-}
-
-setScripts()
-{
-  setTimeout(() => {
-    InitMain.elevateZoomActive();
-    // InitMain.slickSingleProductZoomImage();
-    InitMain.cartPlusMinusButton();
-  }, 200);
-}
-
-addToBasket(id)
-{
-  const cache = JSON.parse(localStorage.getItem(this.auth.getUserUsername()));
-
-  if (this.localArray.length) {
-    this.localArray = [];
-    cache.splice(cache.findIndex(data => data._id == id), 1);
-    if (!cache.length) {
-      localStorage.clear();
-    } else {
-      localStorage.setItem(this.auth.getUserUsername(), JSON.stringify(cache));
+      this.store.setValue('productsQuantity', new Date());
+      return;
     }
+
+    if (cache) {
+      for (let i = 0; i < cache.length; i++) {
+        this.localArray.push(cache[i]);
+      }
+    }
+
+    const obj = {
+      _id: id,
+      quantity: parseInt((<any>document.getElementById('qtybutton')).value)
+    };
+
+    if (!cache || !cache.length || cache.findIndex(data => data._id == id) === -1) {
+      this.localArray.push(obj);
+    }
+
+    localStorage.setItem(this.auth.getUserUsername(), JSON.stringify(this.localArray));
+
     this.store.setValue('productsQuantity', new Date());
-    return;
   }
-
-  if (cache) {
-    for (let i = 0; i < cache.length; i++) {
-      this.localArray.push(cache[i]);
-    }
-  }
-
-  const obj = {
-    _id: id,
-    quantity: parseInt((<any>document.getElementById('qtybutton')).value)
-  };
-
-  if (!cache || !cache.length || cache.findIndex(data => data._id == id) === -1) {
-    this.localArray.push(obj);
-  }
-
-  localStorage.setItem(this.auth.getUserUsername(), JSON.stringify(this.localArray));
-
-  this.store.setValue('productsQuantity', new Date());
-}
 }
