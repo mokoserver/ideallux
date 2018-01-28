2b3fd3966a372543921def62853d7f2a8615b2ea
Johnny <mokoserver@gmail.com>
2018-01-28 22:18:11 +0300

    repush 1 изза корзины

--- src/app/body/basket-card/basket-total-cost/basket-total-cost.component.ts
+++ src/app/body/basket-card/basket-total-cost/basket-total-cost.component.ts
@@ -6,6 +6,7 @@ import { AuthenticationService } from '../../../autentication.service';
   selector: 'app-basket-total-cost',
   templateUrl: './basket-total-cost.component.html',
   styleUrls: ['./basket-total-cost.component.css']
+
 })
 export class BasketTotalCostComponent implements OnInit {
   totalCost;
