2156d0f0c516a41775f8215c56e46cde0f5abe74
Levap4797 <pavelpavlov4797@gmail.com>
2018-02-18 18:59:08 +0300

    свайпер

--- src/app/body/test/test.component.ts
+++ src/app/body/test/test.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {Swiper} from 'swiper';
+
+console.log(Swiper);
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.css'],
 })
+
 export class TestComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100
+  });
+
   }
 
 }
